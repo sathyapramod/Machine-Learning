d4b04459eccc0ab9a14e5dc714b22975d6dae2ed
Sathya pramod <sathyapramod12@gmail.com>
2018-09-23 00:32:08 +0530

    Add files via upload

---
+++ Number.py
@@ -0,0 +1,11 @@
+#Python has "int" "float" "complex" numbers
+
+x = 1
+y = 1.1
+z = 1j
+
+#To verify the type of any object in python use type() function
+
+print(type(x))
+print(type(y))
+print(type(z))
